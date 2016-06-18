fb247cb228448a5cdefa9ee519bf863ef06afce7
Kamil Becmer <kamil.becmer@gmail.com>
2016-06-18 16:15:24 +0200

    Added missing license headers

--- src/main/java/net/maisica/time/interval/TemporalInterval.java
+++ src/main/java/net/maisica/time/interval/TemporalInterval.java
@@ -1,3 +1,18 @@
+/*
+ * Copyright 2016 Kamil Becmer <kamil.becmer at maisica.pl>.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package net.maisica.time.interval;
 
 import java.time.Duration;
@@ -15,6 +30,10 @@ import java.util.stream.Stream;
 import java.util.stream.StreamSupport;
 import net.maisica.time.span.TemporalSpan;
 
+/**
+ *
+ * @author Kamil Becmer <kamil.becmer at maisica.pl>
+ */
 public interface TemporalInterval<T extends Temporal & Comparable<? super T>> extends Interval<T> {
 
     public default Duration toDuration() {
--- src/main/java/net/maisica/time/span/TemporalSpan.java
+++ src/main/java/net/maisica/time/span/TemporalSpan.java
@@ -1,3 +1,18 @@
+/*
+ * Copyright 2016 Kamil Becmer <kamil.becmer at maisica.pl>.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package net.maisica.time.span;
 
 import java.time.Duration;
@@ -15,6 +30,10 @@ import java.util.stream.Stream;
 import java.util.stream.StreamSupport;
 import net.maisica.time.interval.TemporalInterval;
 
+/**
+ *
+ * @author Kamil Becmer <kamil.becmer at maisica.pl>
+ */
 public interface TemporalSpan<T extends Temporal & Comparable<? super T>> extends Span<T, Duration> {
 
     @SuppressWarnings("unchecked")
